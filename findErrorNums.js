e5ca66d6b6d96cf8677039bf706939ba8beaf2a3
Nick Virga <nsvirga@gmail.com>
2024-01-23 16:03:16 -0500

    problem 645 (Set MisMatch) complete - daily question

--- findErrorNums.js
+++ findErrorNums.js
@@ -3,21 +3,31 @@
  * @return {number[]}
  */
 const findErrorNums = (nums) => {
-    let ascending = False
-    if (nums[0] < nums[nums.length-1]) {
-        ascending = True
-    }
+  checkList = [];
+  output = [];
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === nums[i + 1]) {
-      if (nums[i] > i + 1) {
-        return [nums[i], nums[i] - 1];
-      }
-      return [nums[i], nums[i] + 1];
+    checkList[i] = 1;
+  }
+
+  for (let i = 0; i < nums.length; i++) { 
+
+    if (checkList[nums[i] - 1] == 1) {
+      
+      checkList[nums[i] - 1] = 0;
+     
+    } else {
+  
+      checkList[nums[i] - 1] = -1;
+      output[0] = nums[i]
     }
   }
+
+  output[1] = checkList.indexOf(1) + 1
+  return output
 };
 
 console.log(findErrorNums([1, 2, 2, 4])); //[2, 3]
 console.log(findErrorNums([1, 1])); //[1, 2]
 console.log(findErrorNums([2, 2])); //[2, 1]
 console.log(findErrorNums([3, 2, 2])); //[2, 1]
+console.log(findErrorNums([4, 3, 2, 2]));
