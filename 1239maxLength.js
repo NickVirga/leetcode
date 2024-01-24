35eb1dad5e30cd7f0299433112a5dec39e2804b0
Nick Virga <nsvirga@gmail.com>
2024-01-24 14:01:33 -0500

    problem 1239 started - daily problem

---
+++ 1239maxLength.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[]} arr
+ * @return {number}
+ */
+const maxLength = (arr) => {
+  let maxLenConcat = 0;
+  const combos = [];
+  let concat = ""
+
+  for (let groupSize = arr.length; groupSize > 1; groupSize--) {
+    for (let i = 0; i < arr.length - groupSize + 1; i++) {
+      // console.log("i", i);
+      for (let j = i + 1; j < arr.length - groupSize + 2; j++) {
+        //   console.log("j", j);
+        const currCombo = [arr[i]];
+
+        for (let k = 1; k < groupSize; k++) {
+          // console.log("k", k);
+          concat = concat + arr[j + k - 1]
+          currCombo.push(arr[j + k - 1]);
+        }
+
+        console.log(concat)
+        combos.push(currCombo);
+      }
+    }
+  }
+  for (l = 0; l < combos.length; l++) {
+    console.log(combos[l]);
+  }
+};
+
+maxLength(["un", "iq", "ue", "ne", "ss"]);
