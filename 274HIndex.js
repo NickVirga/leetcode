867616e75c46724a15b37bfaa7937f5c1f3bb72c
Nick Virga <nsvirga@gmail.com>
2024-02-02 00:17:37 -0500

    Problem 264 H-Index completed

--- 274HIndex.js
+++ 274HIndex.js
@@ -3,22 +3,51 @@
  * @return {number}
  */
 var hIndex = function(citations) {
-    citations.sort((a,b) => b-a)
-    let maxH = citations.length
-    let hIndex = 0
+    // my solution did not work for all test cases
+    // citations.sort((a,b) => b-a)    
+
+    // let maxHpos = Math.min(citations[0], citations.length)
+    // if (maxHpos === 1) {
+    //     return maxHpos
+    // }
+   
     
+    // for (let i = 1; i < citations.length; i++) {
+    //     // console.log("********************")
+    //     // console.log("i:",i, "value:", citations[i])
+    //     // console.log("maxHpos:", maxHpos)
+             
+    //     if (citations[i] < maxHpos) {
+            
+    //         // maxHpos = Math.min(citations[i], citations.length - i)
+    //         maxHpos = citations[i]
+    //         // console.log("newMaxHPos:", maxHpos)
+    //     }
+
+    //     if (i + 1 >= maxHpos) {
+    //         return maxHpos
+    //     }
+
+    // }
+    // return maxHpos
+
+    citations.sort((a,b) => a-b)
+    
+    const len = citations.length
+
     for (let i = 0; i < citations.length; i++) {
-        console.log("********************")
-        console.log("i:",i, "value:", citations[i])
-        console.log("maxH before:",maxH)
-        if (citations[i] < maxH) {
-            maxH = Math.min(citations.length - i, i + 1)
+        if (citations[i] >= len - i) {
+            return len-i
         }
-        console.log("maxH after:",maxH)
-        
     }
-
-
+    return 0
 };
 
-hIndex([3,0,6,1,5])
+console.log(hIndex([3,0,6,1,5])) //3
+console.log(hIndex([2,0,6,1,5])) //2
+console.log(hIndex([1,3,1])) //1
+console.log(hIndex([0,1])) //1
+console.log(hIndex([4,4,0,0])) //3
+
+
+  
